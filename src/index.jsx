8af305d9c612ded9f0e7aa2ac3ed5b6b77b00285
Oksana-Pak <okspak7@gmail.com>
2023-05-20 11:16:59 +0300

    add 404.html page for correct work

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,9 @@ import { App } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/tweet-cards">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter basename="/tweet-cards">
+    <App />
+  </BrowserRouter>
+  // </React.StrictMode>
 );
