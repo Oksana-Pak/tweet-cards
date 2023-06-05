498ccae050ccb6b57ef586c8d6740d72d64f4297
Oksana-Pak <okspak7@gmail.com>
2023-06-05 23:08:07 +0300

    Updates

---
+++ assets/ShoppingCart-6f0abe19.js
@@ -0,0 +1,32 @@
+import{s as o,L as e,j as t,C as n}from"./index-ce627ad9.js";const i=o.section`
+  padding: 20px 0;
+`;o.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 25px;
+  margin-bottom: 30px;
+`;const r=o(e)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  margin-bottom: 20px;
+  padding: 5px 15px;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.22;
+  color: #373737;
+  &:hover {
+    background: #5cd3a8;
+  }
+`;o.div`
+  position: relative;
+  height: 50px;
+`;const p=()=>t.jsx(i,{children:t.jsx(n,{children:t.jsx(r,{to:"/",children:"Back"})})});export{p as default};
